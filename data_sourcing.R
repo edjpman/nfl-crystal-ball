283ccd727c43724a3d1f4a96ea189ef782fd1267
Ethan <96897006+edjpman@users.noreply.github.com>
2025-06-15 13:10:59 -0700

    Addition of stadium info for weather

--- data_sourcing.R
+++ data_sourcing.R
@@ -1,6 +1,12 @@
 
 library(nflfastR)
-library(dplyr) 
+#library(tidygeocoder)
+#library(httr)
+#library(jsonlite)
+
+
+
+#library(dplyr)
 
 ## Function to save play-by-play data 
 nlf_data_fetch <- function(years,path) {
@@ -27,16 +33,22 @@ read_pbp <- function(path) {
 }
 
 
-data_subset <- function(df,colz) {
+data_subset <- function(df,colz,team) {
     pbp_subset <- df[, c(colz)]
+    pbp_subset <- subset(pbp_subset, grepl(team, pbp_subset$game_id))
     return(pbp_subset)
 }
 
 
 
-file <- "filepath"
+
+
+
+
+
+file <- "/Users/ethanparks/Desktop/repos/pbp_2022_2024.rds"
 yrz <- 2022:2024
-collz <- c('play_id','game_id','home_team','away_team','season_type','week','down','quarter_seconds_remaining',
+collz1 <- c('play_id','game_id','home_team','away_team','season_type','week','down','quarter_seconds_remaining',
 'quarter_end','game_date','posteam','defteam','qtr','time','drive','ydsnet','yards_gained','pass_length','air_yards','yards_after_catch',
 'total_home_score','total_away_score','score_differential','score_differential_post','play_type','posteam_score','defteam_score','posteam_score_post','defteam_score_post',
 'first_down_rush','first_down_pass','first_down_penalty','third_down_converted','third_down_failed','fourth_down_converted','fourth_down_failed','incomplete_pass','penalty',
@@ -46,9 +58,47 @@ collz <- c('play_id','game_id','home_team','away_team','season_type','week','dow
 'temp','wind','stadium_id','game_stadium','play','out_of_bounds')
 
 
+collz2 <- c('play_id','game_id','home_team','away_team','season_type','location','stadium_id','game_stadium')
+
+
 pbp <- read_pbp(path = file)
 
-pbp <- data_subset(df = pbp, colz = collz)
+pbp <- data_subset(df = pbp, colz = collz1, team = "KC")
+
+
+pbp <- data_subset(df = pbp, colz = collz2, team = "KC")
+
+#pbp <- pbp[pbp$season_type == "POST", ]
 
 #print(head(pbp))
-print(colnames(pbp))
+#print(colnames(pbp))
+#print(unique(pbp$week))
+#View(as.data.frame(unique(pbp[, c("stadium_id", "game_stadium")])))
+
+
+stadium_mapping <- data.frame(
+  city = c("New York", "Nashville", "Detroit", "Cincinnati", "Chicago", "Dallas", "Las Vegas",
+           "Green Bay", "Denver", "Jacksonville", "Kansas City", "Baltimore", "Philadelphia",
+           "Pittsburgh", "Cleveland", "San Francisco", "New Orleans", "Buffalo", "Boston",
+           "Tampa", "Indianapolis", "London", "London", "Munich", "Mexico City", "Frankfurt", "São Paulo"),
+  game_stadium = c("MetLife Stadium", "Nissan Stadium", "Ford Field", "Paycor Stadium", "Soldier Field", ##
+              "AT&T Stadium", "Allegiant Stadium", "Lambeau Field", "Empower Field at Mile High", ##
+              "TIAA Bank Stadium", "GEHA Field at Arrowhead Stadium", "M&T Bank Stadium", ##
+              "Lincoln Financial Field", "Acrisure Stadium", "FirstEnergy Stadium", "Levi's Stadium", ##
+              "Mercedes-Benz Superdome", "New Era Field", "Gillette Stadium", "Raymond James Stadium", ##
+              "Lucas Oil Stadium", "Tottenham Stadium", "Wembley Stadium", "Allianz Arena", ##
+              "Azteca Stadium", "Deutsche Bank Park", "Arena Corinthians"), 
+    latitude = c('40.813778','36.166245','42.341563','39.095245','41.862065','32.746202','36.090415','44.502672','39.743563','30.323192','39.048364','39.278440','39.900706','40.447947',
+    '41.504749','37.402606','29.952629','42.775072','42.089961','27.974268','39.758160','51.604572','51.554642','48.217302','19.304293','50.067547','-23.544088'),
+    longitude = c('-74.074310','-86.771141','-83.044846','-84.515875','-87.616560','-97.093053','-115.185056','-88.063807','-105.022415','-81.639100','-94.485532','-76.624118','-75.169016','-80.016837',
+    '-81.699658','-121.971040','-90.081253','-78.789223','-71.265643','-82.504901','-86.162679','-0.066207','-0.279087','11.624038','-99.152843','8.644358','-46.473001'),
+  stringsAsFactors = FALSE
+)
+
+
+
+pbp <- merge(x=pbp, y=stadium_mapping,by='game_stadium', all.x=TRUE)
+
+View(as.data.frame(pbp))
+
+
